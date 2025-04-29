aef8f8a948cc0a1a3504b7b987e3c517b14a6f00
jogunhee <rjsgml771@naver.com>
2025-04-29 18:58:56 +0900

    [Feat] Navigation 추가 (#27)
    * feat: navigation 스토어 및 컴포넌트 추가
    
    * fix: home 테스트 페이지 기존 데이터 제거
    
    * test: storybook 코드 추가

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/header/header'
+import Navigation from '@/components/navigation/navigation'
 
 export const AppLayout = () => {
   return (
@@ -8,6 +9,7 @@ export const AppLayout = () => {
       <main className="pb-[4.25rem]">
         <Outlet />
       </main>
+      <Navigation />
     </>
   )
 }
---
+++ src/components/navigation/navigation.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { MemoryRouter } from 'react-router-dom'
+import Navigation from './navigation'
+import { Tab, useNavigationStore } from '@/stores/navigationStore'
+
+const meta: Meta<typeof Navigation> = {
+  title: 'Components/Navigation',
+  component: Navigation,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+}
+export default meta
+
+type Story = StoryObj<typeof Navigation>
+
+const setSelectedTab = (tab: Tab) => {
+  useNavigationStore.setState({ tab })
+}
+export const HomeSelected: Story = {
+  render: () => {
+    setSelectedTab('home')
+    return <Navigation />
+  },
+}
+
+export const MakeSelected: Story = {
+  render: () => {
+    setSelectedTab('make')
+    return <Navigation />
+  },
+}
+
+export const ResearchSelected: Story = {
+  render: () => {
+    setSelectedTab('research')
+    return <Navigation />
+  },
+}
+
+export const MySelected: Story = {
+  render: () => {
+    setSelectedTab('my')
+    return <Navigation />
+  },
+}
