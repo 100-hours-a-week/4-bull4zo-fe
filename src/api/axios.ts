8a563a0070a72e26b599479eb4c9e99615cdb4cd
gunhee <rjsgml771@naver.com>
2025-04-29 19:04:08 +0900

    feat: axios 설정 인스턴스 추가

---
+++ src/api/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+// auth 인증 없이 사용하는 axios 인스턴스
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5173',
+  timeout: 10000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+// auth 인증 후 사용하는 axios 인스턴스
+export const authAxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5173',
+  timeout: 10000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+  },
+})
