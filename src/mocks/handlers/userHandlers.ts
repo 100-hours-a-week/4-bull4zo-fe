f886403fc513e8000f03661dda8fcc02ecec5232
gunhee <rjsgml771@naver.com>
2025-04-29 19:02:19 +0900

    test: mock 테스트 추가

--- src/mocks/handlers/userHandlers.ts
+++ src/mocks/handlers/userHandlers.ts
@@ -1,10 +1,39 @@
 import { http, HttpResponse } from 'msw'
 
 export const userHandlers = [
-  http.get('/api/v1/users', () => {
-    return HttpResponse.json([
-      { id: 1, name: 'Logan' },
-      { id: 2, name: 'Jenney' },
-    ])
+  http.get('/api/v1/user', async ({ request }) => {
+    const authHeader = request.headers.get('Authorization')
+
+    if (!authHeader) {
+      return HttpResponse.json({ message: 'NO_TOKEN', data: null }, { status: 401 })
+    }
+
+    const token = authHeader.replace('Bearer ', '').trim()
+
+    if (token === 'jwt-access-token') {
+      return HttpResponse.json(
+        {
+          message: 'SUCCESS',
+          data: {
+            nickname: '하와이에서온시나몬롤',
+          },
+        },
+        { status: 200 },
+      )
+    }
+
+    if (token === 'expired-access-token') {
+      return HttpResponse.json({ message: 'TOKEN_EXPIRED', data: null }, { status: 401 })
+    }
+
+    if (token === 'invalid-access-token') {
+      return HttpResponse.json({ message: 'INVALID_TOKEN', data: null }, { status: 401 })
+    }
+
+    if (token === 'user-not-found-access-token') {
+      return HttpResponse.json({ message: 'USER_NOT_FOUND', data: null }, { status: 401 })
+    }
+
+    return HttpResponse.json({ message: 'UNEXPECTED_ERROR', data: null }, { status: 500 })
   }),
 ]
