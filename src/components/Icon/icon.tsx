0a235b9ef9c711e2ec0822477fb8cb5fe031aee4
gunhee <rjsgml771@naver.com>
2025-04-29 19:03:30 +0900

    feat: Icon 컴포넌트 및 카카오 로고 이미지 추가

---
+++ src/components/Icon/icon.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+
+interface IconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  size?: number | string // size를 숫자(px) 또는 문자열(예: 2rem)로 받음
+}
+
+const Icon = ({ src, alt, size = 24, className, ...props }: IconProps) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={typeof size === 'number' ? size : undefined}
+      height={typeof size === 'number' ? size : undefined}
+      style={typeof size === 'string' ? { width: size, height: size } : undefined}
+      className={cn('object-contain', className)}
+      {...props}
+    />
+  )
+}
+
+export { Icon }
