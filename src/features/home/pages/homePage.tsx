d28e41d2d19e4e3004011a35ae36167afff18552
gunhee <rjsgml771@naver.com>
2025-05-01 21:53:36 +0900

    feat: 모달 연결 및 스와이프 카드 연결 (#30)

--- src/features/home/pages/homePage.tsx
+++ src/features/home/pages/homePage.tsx
@@ -1,5 +1,5 @@
 import { useInfiniteVotesQuery } from '@/api/services/vote/quries'
-import { VoteNoMoreCard } from '@/components/card/voteCard'
+import { VoteNoMoreCard } from '@/components/card/voteNoMoreCard'
 import { GroupDropDown } from '@/components/dropdown/groupDropDown'
 import { LoadingCard } from '@/components/loading/loadingCard'
 import { Modal } from '@/components/modal/modal'
@@ -7,14 +7,13 @@ import { NoVoteAvailAbleModal } from '@/components/modal/noVoteAvailableModal'
 import { useGroupStore } from '@/stores/groupStore'
 import { useModalStore } from '@/stores/modalStore'
 import { useUserStore } from '@/stores/userStore'
-import VoteSwiper from '../components/voteSwiper'
+import VoteSwiperFramer from '../components/voteSwiper_framer'
 
 const HomePage = () => {
   const isLogin = useUserStore((state) => state.isLogin)
   const groupId = useGroupStore((state) => state.selectedId)
-  const { data, isSuccess, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteVotesQuery(
-    { groupId, isLogin },
-  )
+  const { data, isSuccess, isFetching, isError, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteVotesQuery({ groupId, isLogin })
 
   const { isOpen, openModal } = useModalStore()
 
@@ -29,25 +28,37 @@ const HomePage = () => {
   if (isOpen) {
     return <Modal />
   }
-
+  // 로딩 카드
+  if (isLogin || isFetching) {
+    return (
+      <div className="flex h-full justify-center items-center">
+        <LoadingCard />
+      </div>
+    )
+  }
+  // 더 이상 진행할 카드가 없는 경우 (에러)
+  if (isError) {
+    return (
+      <div className="flex h-full justify-center items-center">
+        <VoteNoMoreCard />
+      </div>
+    )
+  }
   return (
-    <article className="px-5 py-4 overflow-hidden h-full relative">
-      {isLogin && <GroupDropDown />}
-      {!isSuccess && (
-        <div className="flex justify-center items-center">
-          <LoadingCard />
+    <article className=" overflow-hidden h-full relative">
+      {isLogin && (
+        <div className="absolute z-10 left-4 top-3">
+          <GroupDropDown />
         </div>
       )}
-      {isSuccess && data?.pages ? (
-        <VoteSwiper
-          pages={data.pages}
-          fetchNextPage={FetchNextPage}
-          hasNextPage={hasNextPage}
-          isFetchingNextPage={isFetchingNextPage}
-        />
-      ) : (
-        <div>
-          <VoteNoMoreCard />
+      {isSuccess && data?.pages && (
+        <div className=" w-full h-full">
+          <VoteSwiperFramer
+            pages={data.pages}
+            fetchNextPage={FetchNextPage}
+            hasNextPage={hasNextPage}
+            isFetchingNextPage={isFetchingNextPage}
+          />
         </div>
       )}
     </article>
