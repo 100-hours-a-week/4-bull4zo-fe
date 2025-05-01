76ae9742cee354905f9ce3e08cd52e0234819919
gunhee <rjsgml771@naver.com>
2025-05-01 17:30:18 +0900

    feat: 스와이핑 개발 및 모달 컴포넌트 연동 (#30)

--- src/features/home/pages/homePage.tsx
+++ src/features/home/pages/homePage.tsx
@@ -1,9 +1,56 @@
+import { useInfiniteVotesQuery } from '@/api/services/vote/quries'
+import { VoteNoMoreCard } from '@/components/card/voteCard'
 import { GroupDropDown } from '@/components/dropdown/groupDropDown'
+import { LoadingCard } from '@/components/loading/loadingCard'
+import { Modal } from '@/components/modal/modal'
+import { NoVoteAvailAbleModal } from '@/components/modal/noVoteAvailableModal'
+import { useGroupStore } from '@/stores/groupStore'
+import { useModalStore } from '@/stores/modalStore'
 import { useUserStore } from '@/stores/userStore'
+import VoteSwiper from '../components/voteSwiper'
 
 const HomePage = () => {
   const isLogin = useUserStore((state) => state.isLogin)
+  const groupId = useGroupStore((state) => state.selectedId)
+  const { data, isSuccess, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteVotesQuery(
+    { groupId, isLogin },
+  )
 
-  return <article className="px-5 py-4">{isLogin && <GroupDropDown />}</article>
+  const { isOpen, openModal } = useModalStore()
+
+  const FetchNextPage = () => {
+    if (!isLogin) {
+      openModal(<NoVoteAvailAbleModal />)
+      return
+    }
+    fetchNextPage()
+  }
+
+  if (isOpen) {
+    return <Modal />
+  }
+
+  return (
+    <article className="px-5 py-4 overflow-hidden h-full relative">
+      {isLogin && <GroupDropDown />}
+      {!isSuccess && (
+        <div className="flex justify-center items-center">
+          <LoadingCard />
+        </div>
+      )}
+      {isSuccess && data?.pages ? (
+        <VoteSwiper
+          pages={data.pages}
+          fetchNextPage={FetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+        />
+      ) : (
+        <div>
+          <VoteNoMoreCard />
+        </div>
+      )}
+    </article>
+  )
 }
 export default HomePage
