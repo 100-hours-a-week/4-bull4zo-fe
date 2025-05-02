40ff15f09ef2c5ae9a1f1e4c0395c7d2ca5cc1e7
jogunhee <rjsgml771@naver.com>
2025-05-02 16:08:33 +0900

    [Feat] 투표등록 페이지 개발 (#46)
    * feat: 미리보기 카드 개발(#31)
    
    * feat: 미리보기 전용 모달 개발(#31)
    
    * feat 투표등록(make) 페이지 개발 (#31)

---
+++ src/features/make/components/voteCardPreviewModal.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { useCreateVoteMutation } from '@/api/services/vote/quries'
+import { useModalStore } from '@/stores/modalStore'
+import { VoteCardPreview } from '../../../components/card/voteCardPreview'
+import { Button } from '../../../components/ui/button'
+
+type Props = {
+  groupId: number
+  content: string
+  image?: File
+  closedAt: string
+}
+
+export const VoteCardPreviewModal = ({ groupId, content, image, closedAt }: Props) => {
+  const navigation = useNavigate()
+
+  const { closeModal } = useModalStore()
+  const { mutate } = useCreateVoteMutation()
+
+  const onSubmit = () => {
+    mutate(
+      { groupId, content, imageUrl: '', closedAt },
+      {
+        onSuccess: () => {
+          navigation('/research')
+        },
+        onError: () => {
+          toast('잘못된 값이 입력되었습니다.')
+        },
+      },
+    )
+    closeModal()
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-full">
+      <VoteCardPreview content={content} image={image} closedAt={closedAt} />
+      <div className="flex gap-4 mt-4">
+        <Button onClick={() => closeModal()}>닫기</Button>
+        <Button onClick={onSubmit}>등록</Button>
+      </div>
+    </div>
+  )
+}
