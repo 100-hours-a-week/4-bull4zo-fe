9a4de0100d1143cba0978459e08def1e31d41b64
gunhee <rjsgml771@naver.com>
2025-05-04 15:09:35 +0900

    feat: 투표 참여 리스트 개발(투표 결과 페이지 개발 #32)

---
+++ src/features/research/components/researchList.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import {
+  useCreateVotesInfinityQuery,
+  useParticipatedVotesInfinityQuery,
+} from '@/api/services/vote/quries'
+import { VoteList } from '@/components/list/voteList'
+import { Label } from '@/components/ui/label'
+import { useGroupStore } from '@/stores/groupStore'
+
+export const ResearchList = () => {
+  const [index, setIndex] = useState(0)
+  const { selectedId } = useGroupStore()
+
+  const participatedQuery = useParticipatedVotesInfinityQuery({
+    groupId: selectedId,
+  })
+
+  const myVotesQuery = useCreateVotesInfinityQuery({
+    groupId: selectedId,
+  })
+
+  const data = index === 0 ? participatedQuery.data : myVotesQuery.data
+  const fetchNextPage = index === 0 ? participatedQuery.fetchNextPage : myVotesQuery.fetchNextPage
+  const hasNextPage = index === 0 ? participatedQuery.hasNextPage : myVotesQuery.hasNextPage
+  const isFetchingNextPage =
+    index === 0 ? participatedQuery.isFetchingNextPage : myVotesQuery.isFetchingNextPage
+
+  return (
+    <section className="px-4 py-2 bg-white pb-20">
+      <div className="flex items-center justify-center gap-2">
+        <Label
+          onClick={() => setIndex(0)}
+          className={`text-2xl font-unbounded cursor-pointer ${index === 0 ? 'font-bold' : 'font-light'}`}
+        >
+          참여한 투표
+        </Label>
+        <Label className="text-xl font-unbounded">|</Label>
+        <Label
+          onClick={() => setIndex(1)}
+          className={`text-2xl font-unbounded cursor-pointer ${index === 1 ? 'font-bold' : 'font-light'}`}
+        >
+          내가 만든 투표
+        </Label>
+      </div>
+      <VoteList
+        data={data}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isFetchingNextPage={isFetchingNextPage}
+      />
+    </section>
+  )
+}
