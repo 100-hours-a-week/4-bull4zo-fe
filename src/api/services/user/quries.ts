0cf7efb01d759a39bf20ee8a34a6ab6fd6c2dbae
gunhee <rjsgml771@naver.com>
2025-05-05 13:11:35 +0900

    feat: 회원 수정, 탈퇴 캐싱 추가

--- src/api/services/user/quries.ts
+++ src/api/services/user/quries.ts
@@ -1,11 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { userService } from './service'
 
+// 유저 정보 조회
 export const useUserInfoQuery = () => {
   return useQuery({
     queryKey: ['user'],
     queryFn: userService.getUserInfo,
     staleTime: 1000 * 60 * 5,
     retry: 1,
+  })
+}
+// 유저 정보 수정
+export const useUserUpdateMutation = () => {
+  return useMutation({
+    mutationFn: userService.updateUser,
+  })
+}
+// 유저 로그아웃
+export const useUserLogoutMutation = () => {
+  return useMutation({
+    mutationFn: userService.logout,
+  })
+}
+// 유저 탈퇴
+export const useUserDeleteMutation = () => {
+  return useMutation({
+    mutationFn: userService.deleteUser,
   })
 }
