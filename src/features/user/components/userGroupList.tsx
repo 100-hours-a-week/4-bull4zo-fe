6d865f8ac152c8c72f0c0851410d9de0d428f213
gunhee <rjsgml771@naver.com>
2025-05-06 12:42:15 +0900

    feat: 그룹 초대 모달 연결

--- src/features/user/components/userGroupList.tsx
+++ src/features/user/components/userGroupList.tsx
@@ -1,15 +1,23 @@
 import { useInfiniteGroupsQuery } from '@/api/services/user/group/quries'
 import { GroupList } from '@/components/list/groupList'
+import { AddGroupModal } from '@/components/modal/addGroupModal'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
+import { useModalStore } from '@/stores/modalStore'
 
 export const UserGroupList = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteGroupsQuery()
+  const { openModal } = useModalStore()
 
   return (
     <section className="mt-8">
       <Label className="text-2xl font-bold font-unbounded">그룹</Label>
-      <Button className="bg-gray-300 mt-3 w-full text-5xl h-full text-white">+</Button>
+      <Button
+        onClick={() => openModal(<AddGroupModal />)}
+        className="bg-gray-300 mt-3 w-full text-5xl h-full text-white"
+      >
+        +
+      </Button>
       <GroupList
         data={data}
         fetchNextPage={fetchNextPage}
