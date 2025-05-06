770a0723b521607e388f393ddd725cb8e8c1cc20
gunhee <rjsgml771@naver.com>
2025-05-06 12:43:12 +0900

    feat: 그룹 초대코드 스키마 추가

--- src/features/user/lib/userSchema.ts
+++ src/features/user/lib/userSchema.ts
@@ -4,3 +4,8 @@ export const nicknameSchema = z.object({
   nickname: z.string().min(2, '2자 이상 10자 이하').max(10, '2자 이상 10자 이하'),
 })
 export type NicknameSchema = z.infer<typeof nicknameSchema>
+
+export const inviteCodeSchema = z.object({
+  inviteCode: z.string().min(6, '6자 이상 8자 이하').max(8, '6자 이상 8자 이하'),
+})
+export type InviteCodeSchema = z.infer<typeof inviteCodeSchema>
