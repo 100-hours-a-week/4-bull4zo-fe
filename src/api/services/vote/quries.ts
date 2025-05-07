b079e4572bcba90ebfde7838ec23408a26b261c3
gunhee <rjsgml771@naver.com>
2025-05-07 09:42:00 +0900

    feat: 투표 방향 라벨 추가

--- src/api/services/vote/quries.ts
+++ src/api/services/vote/quries.ts
@@ -20,7 +20,7 @@ export const useInfiniteVotesQuery = ({
   const effectiveGroupId = isLogin ? groupId : 1
 
   return useInfiniteQuery<VoteData, AxiosError>({
-    queryKey: ['votes', effectiveGroupId, effectiveSize],
+    queryKey: ['votes'],
     queryFn: ({ pageParam }) =>
       voteService.getVotes({
         groupId: effectiveGroupId,
