abeced246fe8db99e6728637cfcd9b3bba0de955
gunhee <rjsgml771@naver.com>
2025-05-07 16:19:20 +0900

    feat: 자동 재 로그인로직 추가

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { userService } from '@/api/services/user/service'
 import Header from '@/components/header/header'
 import Navigation from '@/components/navigation/navigation'
 import { useNavigationStore } from '@/stores/navigationStore'
@@ -9,7 +11,7 @@ export const AppLayout = () => {
   const location = useLocation()
   const navigation = useNavigate()
   const { setTab } = useNavigationStore()
-  const { isLogin } = useUserStore()
+  const { isLogin, setAccessToken } = useUserStore()
 
   useEffect(() => {
     if (!isLogin) {
@@ -17,6 +19,27 @@ export const AppLayout = () => {
     }
     setTab(location.pathname)
   }, [location.pathname, setTab, isLogin, navigation])
+
+  useEffect(() => {
+    const checkAndRefreshToken = async () => {
+      const storedAccessToken = useUserStore.getState().accessToken
+
+      if (!storedAccessToken) {
+        try {
+          const newAccessToken = await userService.refreshAccessToken()
+          setAccessToken(newAccessToken)
+          // eslint-disable-next-line no-unused-vars
+        } catch (e) {
+          toast('토큰 갱신 실패')
+          navigation('/home')
+          return
+        }
+      }
+    }
+
+    checkAndRefreshToken()
+    setTab(location.pathname)
+  }, [location.pathname, setTab, navigation])
 
   return (
     <>
