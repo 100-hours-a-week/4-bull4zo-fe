76eaf17f7d85018adf5f2e8773a232434dbeebb4
gunhee <rjsgml771@naver.com>
2025-05-19 15:47:00 +0900

    refactor: 코드 축약

--- src/features/research/components/researchList.tsx
+++ src/features/research/components/researchList.tsx
@@ -26,33 +26,28 @@ export const ResearchList = () => {
   const isFetchingNextPage =
     index === 0 ? participatedQuery.isFetchingNextPage : myVotesQuery.isFetchingNextPage
 
+  const onClickHandler = (index: number) => {
+    setIndex(index)
+    trackEvent({
+      cta_id: 'vote_research_tab',
+      action: 'tab',
+      page: location.pathname,
+    })
+  }
+
   return (
     <section className="px-7 py-2">
       <div className="flex items-center justify-around mb-4">
         <div className="" />
         <Label
-          onClick={() => {
-            setIndex(0)
-            trackEvent({
-              cta_id: 'vote_research_tab',
-              action: 'tab',
-              page: location.pathname,
-            })
-          }}
+          onClick={() => onClickHandler(0)}
           className={`text-2xl flex items-center justify-end font-unbounded cursor-pointer w-35 ${index === 0 ? 'font-bold' : 'font-light'}`}
         >
           참여한 투표
         </Label>
         <div className="h-6 bg-black w-[1px]" />
         <Label
-          onClick={() => {
-            setIndex(1)
-            trackEvent({
-              cta_id: 'vote_research_tab',
-              action: 'tab',
-              page: location.pathname,
-            })
-          }}
+          onClick={() => onClickHandler(1)}
           className={`text-2xl font-unbounded cursor-pointer ${index === 1 ? 'font-bold' : 'font-light'}`}
         >
           내가 만든 투표
