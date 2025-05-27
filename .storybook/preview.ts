ec2ec14c267b24edb2e6a246dcfbae301acb18df
gunhee <rjsgml771@naver.com>
2025-05-27 11:35:18 +0900

    feat: storybook 내 msw 설정 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,9 @@
 import { type Preview } from '@storybook/react'
+import { initialize, mswDecorator, mswLoader } from 'msw-storybook-addon'
+import { handlers } from '../src/mocks/handlers'
 import '../src/styles/globals.css'
+
+initialize()
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +13,12 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers,
+    },
   },
+  decorators: [mswDecorator],
+  loaders: [mswLoader],
 }
 
 export default preview
