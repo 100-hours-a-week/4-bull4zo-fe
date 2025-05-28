cd0dd4ad89d63a11943f6f9d82dbbf8227e761ef
gunhee <rjsgml771@naver.com>
2025-05-28 15:33:35 +0900

    feat: 누락된 튜토리얼 hide 설정 변경

---
+++ src/app/NotFound.tsx
@@ -0,0 +1,16 @@
+import { Icon } from '@/components/Icon/icon'
+import MOA_404 from '../../public/moa_404.svg?react'
+
+const NotFoundPage = () => {
+  return (
+    <section className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+      <Icon component={MOA_404} className="h-[300px] w-[300px]" />
+      <h1 className="text-xl text-center">
+        현재 서비스 <span className="text-primary font-bold">준비중</span> 입니다. <br />
+        다음에 다시 이용해 주세요!
+      </h1>
+    </section>
+  )
+}
+
+export default NotFoundPage
