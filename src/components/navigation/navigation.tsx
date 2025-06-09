6d9967c8c7e86797c32bd396326e5f7f91082750
gunhee <rjsgml771@naver.com>
2025-06-09 11:04:06 +0900

    feat: 알림 컴포넌트 및 기능 추가(#177)

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -55,7 +55,7 @@ const NavigationItem = ({ icon, label }: NavigationItemProps) => {
 
 const Navigation = () => {
   return (
-    <nav className="flex z-[999] flex-row items-center justify-around max-w-[450px] w-full h-[4rem] fixed bottom-0 bg-white">
+    <nav className="flex z-50 flex-row items-center justify-around max-w-[450px] w-full h-[4rem] fixed bottom-0 bg-white">
       <NavigationItem icon={<Icon component={Vote} />} label="home" />
       <NavigationItem icon={<Plus />} label="make" />
       <NavigationItem icon={<ListCheckIcon />} label="research" />
