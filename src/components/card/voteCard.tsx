8401feb12bbd3c58391d280b8a77763ae4a41513
gunhee <gunhee.jo.dev@gmail.com>
2025-07-01 14:53:43 +0900

    refactor: votecard 컴파운드로 변경

--- src/components/card/voteCard.tsx
+++ src/components/card/voteCard.tsx
@@ -1,80 +1,29 @@
-import { useEffect, useState } from 'react'
 import { Vote } from '@/api/services/vote/model'
 import META_ICON from '@/assets/meta_icon.webp'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/index'
-import { cn } from '@/lib/utils'
+import { VCard } from '@/components/index'
 import { useGroupStore } from '@/stores/index'
 import { formatRelativeTime } from '@/utils/time'
 import { Icon } from '../Icon/icon'
 
 export const VoteCard = (props: Partial<Vote>) => {
-  const [isImageValid, setIsImageValid] = useState(false)
   const { selectedId: groupId } = useGroupStore()
 
-  useEffect(() => {
-    if (!props.imageUrl) return setIsImageValid(false)
-
-    const img = new Image()
-    img.src = props.imageUrl
-    img.onload = () => setIsImageValid(true)
-    img.onerror = () => setIsImageValid(false)
-  }, [props.imageUrl])
-
   return (
-    <Card
-      className={cn(
-        `p-0 w-[80%] h-[30rem] max-h-[75%] border-none  pointer-events-auto text-white rounded-[3.125rem] shadow-card text-shadow-lg`,
-        !isImageValid && 'bg-primary-gradient-down',
-      )}
-      style={
-        isImageValid
-          ? {
-              backgroundImage: `url(${props.imageUrl})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-            }
-          : undefined
-      }
-    >
-      <CardHeader className="flex flex-row justify-between px-4 pt-9">
-        <div className="flex flex-row gap-1">
-          <div className="flex flex-col gap-1">
-            <div className="flex flex-row items-center gap-2">
-              <CardTitle className="font-pyeojinGothic text-xl line-clamp-1">
-                {props.authorNickname}
-              </CardTitle>
-              {props.adminVote === 1 && (
-                <Icon className="items-start" src={META_ICON} alt="공인 뱃지" size={20} />
-              )}
-              {props.voteType === 'AI' && (
-                <div className="rounded-full bg-primary size-5 flex items-center justify-center border-white border-[1px] text-sm shadow-md">
-                  AI
-                </div>
-              )}
-            </div>
-            {groupId === 0 && <span className="text-sm line-clamp-1">{props.groupName}</span>}
-          </div>
-        </div>
-        <span className="text-xs pr-2">{formatRelativeTime(props.closedAt as string)}</span>
-      </CardHeader>
-      <CardContent className="flex-1 relative overflow-hidden px-4 pb-9">
-        {isImageValid && (
-          <div className="absolute inset-0 bg-gradient-to-t from-black/40 via-black/40 to-transparent rounded-[3.125rem]" />
-        )}
-        <div
-          className={cn(
-            'min-h-[90%] flex items-end justify-center px-2',
-            !isImageValid && 'items-center',
-          )}
+    <VCard.Root image={props.imageUrl} isPreview={false}>
+      <VCard.Header>
+        <VCard.Author
+          name={props.authorNickname ?? ''}
+          badge={props.adminVote === 1 && <Icon src={META_ICON} alt="공인 뱃지" size={20} />}
+          isAI={props.voteType === 'AI'}
         >
-          <p
-            data-testid="vote-content"
-            className="sm:text-xl whitespace-pre-line break-all text-center py-2 z-30"
-          >
-            {props.content}
-          </p>
-        </div>
-      </CardContent>
-    </Card>
+          {groupId === 0 && <VCard.Group name={props.groupName} />}
+        </VCard.Author>
+        <VCard.ClosedAt date={formatRelativeTime(props.closedAt ?? '')} />
+      </VCard.Header>
+      <VCard.Content>
+        <VCard.Overlay />
+        <VCard.Body>{props.content}</VCard.Body>
+      </VCard.Content>
+    </VCard.Root>
   )
 }
--- src/components/card/voteCardPreview.tsx
+++ src/components/card/voteCardPreview.tsx
@@ -1,7 +1,4 @@
-// import { useEffect, useState } from 'react'
 import { VCard } from '@/components/index'
-// import { Card, CardContent, CardHeader, CardTitle } from '@/components/index'
-// import { cn } from '@/lib/utils'
 import { useUserStore } from '@/stores/index'
 import { formatRelativeTime } from '@/utils/time'
 
@@ -12,91 +9,10 @@ type Props = {
   anonymous: boolean
 }
 
-// const usePreviewImageUrl = (file?: File | string) => {
-//   const [url, setUrl] = useState<string | null>(null)
-
-//   useEffect(() => {
-//     if (!file) {
-//       setUrl(null)
-//       return
-//     }
-//     if (typeof file === 'string') {
-//       setUrl(file)
-//       return
-//     }
-
-//     const objectUrl = URL.createObjectURL(file)
-//     setUrl(objectUrl)
-
-//     return () => URL.revokeObjectURL(objectUrl)
-//   }, [file])
-
-//   return url
-// }
-
 export const VoteCardPreview = ({ content, image, closedAt, anonymous }: Props) => {
   const { nickname } = useUserStore()
-  // const imageUrl = usePreviewImageUrl(image)
-  // const [isImageValid, setIsImageValid] = useState(false)
-
-  // useEffect(() => {
-  //   if (!imageUrl) return setIsImageValid(false)
-
-  //   const img = new Image()
-  //   img.src = imageUrl
-  //   img.onload = () => setIsImageValid(true)
-  //   img.onerror = () => setIsImageValid(false)
-  // }, [imageUrl])
 
   return (
-    // <Card
-    //   className={cn(
-    //     `p-0 w-[80%] h-[30rem] max-h-[75%] border-none pointer-events-auto text-white rounded-[3.125rem] shadow-card text-shadow-lg`,
-    //     !isImageValid && 'bg-primary-gradient-down',
-    //   )}
-    //   style={
-    //     isImageValid
-    //       ? {
-    //           backgroundImage: `url(${imageUrl})`,
-    //           backgroundSize: 'cover',
-    //           backgroundPosition: 'center',
-    //         }
-    //       : undefined
-    //   }
-    // >
-    //   <CardHeader className="flex flex-row justify-between px-4 pt-9">
-    //     {!anonymous ? (
-    //       <div className="flex flex-row gap-1">
-    //         <CardTitle className="font-pyeojinGothic text-xl line-clamp-1">{nickname}</CardTitle>
-    //         {/* <Icon src={META_ICON} alt="공인 뱃지" size={20} /> */}
-    //       </div>
-    //     ) : (
-    //       <div className="flex flex-row gap-1">
-    //         <CardTitle className="font-pyeojinGothic text-2xl">익명</CardTitle>
-    //       </div>
-    //     )}
-    //     <span className="text-xs pr-2 min-w-20">{formatRelativeTime(closedAt, false)}</span>
-    //   </CardHeader>
-    //   <CardContent className="flex-1 relative overflow-hidden px-4 pb-9">
-    //     {isImageValid && (
-    //       <div className="absolute inset-0 bg-gradient-to-t from-black/40 via-black/40 to-transparent rounded-[3.125rem]" />
-    //     )}
-
-    //     <div
-    //       className={cn(
-    //         'min-h-[90%] flex items-end justify-center px-2',
-    //         !isImageValid && 'items-center',
-    //       )}
-    //     >
-    //       <p
-    //         data-testid="vote-content"
-    //         className="sm:text-xl whitespace-pre-line break-all text-center py-2 z-30"
-    //       >
-    //         {content}
-    //       </p>
-    //     </div>
-    //   </CardContent>
-    // </Card>
     <VCard.Root image={image}>
       <VCard.Header>
         <VCard.Author name={!anonymous ? nickname : '익명'} badge={null} />
