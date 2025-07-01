e387a1bc60ff45284e06eab58bbed8672f10e2f1
gunhee <gunhee.jo.dev@gmail.com>
2025-07-01 16:11:49 +0900

    refactor: 컴파운드 디자인 패턴 적용

--- src/components/card/vote/voteNoMoreCard.tsx
+++ src/components/card/vote/voteNoMoreCard.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import { Button, Card, CardContent, CardHeader, CardTitle } from '@/components/index'
+import { Button, VCard } from '@/components/index'
 import { useUserStore } from '@/stores/index'
 
 export const VoteNoMoreCard = () => {
@@ -7,32 +7,27 @@ export const VoteNoMoreCard = () => {
   const { isLogin } = useUserStore()
 
   return (
-    <Card
-      className={`px-4 py-9 w-[80%] h-[30rem] max-h-[75%] flex justify-center bg-primary-gradient-down text-white rounded-[3.125rem] shadow-card relative`}
-    >
-      <CardHeader className="flex flex-col justify-between w-full items-center px-0 text-black">
-        <div className="flex flex-row gap-1 ">
-          <CardTitle className="font-unbounded text-2xl text-center">
-            지금은 잠잠하네요... <br /> 새로운 투표가 없습니다.
-          </CardTitle>
-        </div>
-      </CardHeader>
-      <CardContent className="flex flex-col justify-around items-center text-black">
-        <div className="w-full flex justify-around">
-          <Button
-            className="px-5"
-            disabled={!isLogin}
-            onClick={() => {
-              navigation('/make')
-            }}
-          >
-            새 투표 만들기
-          </Button>
-        </div>
-        <div className=" absolute bottom-8 text-[0.75rem] text-center">
-          <p>💬 그룹을 '전체'로 바꾸면 더 많은 투표를 볼 수 있어요!</p>
-        </div>
-      </CardContent>
-    </Card>
+    <VCard.ModalRoot>
+      <VCard.ModalHeader>
+        <VCard.ModalTitle>
+          지금은 잠잠하네요...
+          <br /> 새로운 투표가 없습니다.
+        </VCard.ModalTitle>
+      </VCard.ModalHeader>
+      <VCard.ModalContent>
+        <Button
+          className="px-5"
+          disabled={!isLogin}
+          onClick={() => {
+            navigation('/make')
+          }}
+        >
+          새 투표 만들기
+        </Button>
+        <VCard.ModalFooter>
+          💬 그룹을 '전체'로 바꾸면 더 많은 투표를 볼 수 있어요!
+        </VCard.ModalFooter>
+      </VCard.ModalContent>
+    </VCard.ModalRoot>
   )
 }
