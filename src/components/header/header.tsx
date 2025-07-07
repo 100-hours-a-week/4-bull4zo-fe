a9c8b87ad12660ae9dcca934e11827dfdf6e644a
gunhee <gunhee.jo.dev@gmail.com>
2025-07-07 10:04:05 +0900

    fix: 헤더 알림 트리거 업데이트

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import { FaAngleLeft } from 'react-icons/fa6'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Bell } from 'lucide-react'
+import { useInfiniteNotificationQuery } from '@/api/services/notification/queries'
 import MOA_HOME_ICON from '@/assets/moa_home.webp'
 import { useNotificationStore } from '@/stores/notificationStore'
 import { useSliderStore } from '@/stores/sliderStore'
@@ -14,6 +15,7 @@ const Header = () => {
   const { open } = useSliderStore()
   const { newNotification } = useNotificationStore()
   const { isOpen } = useSliderStore()
+  const { data } = useInfiniteNotificationQuery()
 
   const location = useLocation()
 
@@ -21,9 +23,14 @@ const Header = () => {
   const excludedPaths = ['/home', '/make', '/research', '/user', '/auth/callback']
   const showBackButton = !excludedPaths.includes(path)
 
+  const hasUnread = data?.pages
+    ?.flatMap((page) => page.notifications)
+    ?.some((notification) => notification.read === 0)
+
   const handleBack = () => {
     router(-1)
   }
+
   return (
     <header className=" fixed w-full max-w-[450px] overflow-hidden shadow-header flex flex-row justify-between items-center z-50 bg-white">
       {showBackButton && (
@@ -50,16 +57,7 @@ const Header = () => {
                 open()
               }}
             />
-            {/* {hasUnread && (
-              <span className="absolute -top-1 right-0 flex size-3">
-                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-                <span
-                  className="relative inline-flex size-3 rounded-full bg-red-500"
-                  onClick={open}
-                />
-              </span>
-            )} */}
-            {!isOpen && newNotification && (
+            {!isOpen && (newNotification || hasUnread) && (
               <span className="absolute -top-1 right-0 flex size-3">
                 <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
                 <span
